d90edb057cd74ffeda47af564eac0a478aabd703
sudheerIITTPTY <cs19m005@iittp.ac.in>
2020-03-09 01:56:00 +0530

    functions folder was created and simple addition for returning sum of two numbers by taking two arugumets was added to the folder.

---
+++ functions/additon_function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 01:52:20 2020
+
+@author: Debugger
+"""
+
+# defining a simple addtion.
+# It will take 2 numbers as input and return their sum
+def addition(a,b):
+    return a+b
+
+#calling addition function
+print(addition(2,3))
